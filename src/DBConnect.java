b26bb894c66ff1afb26174d8b30f99818722783d
Hamsheed <hamsheed993@gmail.com>
2017-04-21 10:04:30 +0400

    first commit

---
+++ src/DBConnect.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class DBConnect {
+	
+	static String url = "";
+	static String user = "";
+	static String password = "";
+	static Connection con;
+	
+	public static void DoConnect() throws SQLException, ClassNotFoundException {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		 con = DriverManager.getConnection(url, user, password);
+		
+	}
+}
